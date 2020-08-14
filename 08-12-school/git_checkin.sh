c039db8d7d233cc3d07fd4818e223a17e8072aa6
you you <you@example.com>
2020-08-14 14:55:43 -0700

    adding bash script

--- 08-12-school/git_checkin.sh
+++ 08-12-school/git_checkin.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 fs=48000
-for chirp_dur in 12.5 25 50 75 100
+for chirp_dur in 25 50 75 100
 do
 for trial in 1 2 # # 3
 do
-    for inch in 60 #72 #36 # #36 #12 48 60 72 84 96 108 120 132 144 
+    for inch in 36 #72 #36 # #36 #12 48 60 72 84 96 108 120 132 144 
     do
-	    #fmcw_10k_15k_100_36inch_tegr0812_samedev_1_mic_48000_raw_s16le_channel_0.raw
-	    #fmcw_5k_7k_100_36inch_tegr0812_sep_16000_1_mic_16000_raw_s16le_channel_0.raw
 	    for prefix in fmcw_10k_15k_${chirp_dur} # fmcw_16k_18k_30 fmcw_16k_20k_30
 	    do
         	prefix2=${prefix}_${inch}inch_tegr0812_samedev_${trial}
@@ -16,12 +14,14 @@ do
             	    rawfile=${prefix2}_mic_${fs}_raw_s16le_channel_${ch}.raw
 	    	    wavfile=./tegr0812_wav/${prefix2}_${fs}_channel_${ch}.wav
 	    	    sox -r ${fs} -c 1 -e signed -b 16 ${rawfile} ${wavfile} 
-                    git add ${wavfile}
-	            git commit -m "adding wav file" ${wavfile}
+                    
+		    #git add ${wavfile}
+	            
+		    #git commit -m "adding wav file" ${wavfile}
                 done
             done
     done
     done
 done
-git push
+#git push
 
